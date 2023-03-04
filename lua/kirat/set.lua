db52d4461cc35980ab7f65de3c36d79b57645e98
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2023-03-03 17:07:31 -0800

    Fix some default options

--- lua/kirat/set.lua
+++ lua/kirat/set.lua
@@ -1,10 +1,12 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+vim.opt.wrap = false -- don't wrap lines
 
 vim.opt.smartindent = true
 
@@ -16,3 +18,9 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
